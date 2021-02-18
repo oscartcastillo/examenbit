2c67f23d39b89be7ba9fa71aa1335af6830e019c
oscartcastillo <49007213+oscartcastillo@users.noreply.github.com>
2021-02-18 13:05:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,6 @@
 	<h5>Precio <span class="eur"></span> <span class="eur_new" style="display: none;"></span></h5>
 
 
-
-
-
-
 	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
@@ -39,11 +35,11 @@
 	<script>
 
 
-	/*inicio();
+	inicio();
 
 	function inicio(){
 		$.ajax({
-			url     : "consume.php",
+			url     : "src/consume.php",
 			type    : "get",
 			dataType : "json",
 			success : (function (data) {
@@ -52,8 +48,7 @@
 				$('.eur').text(data.EUR).css('color', 'black');	
 			})
 		});
-
-	}*/
+	}
 
     setInterval(comprueba, 2000);
 
@@ -61,7 +56,7 @@
 
 
     	$.ajax({
-			url     : "consume.php",
+			url     : "src/consume.php",
 			type    : "get",
 			dataType : "json",
 			success : (function (data) {
@@ -112,12 +107,6 @@
 			})
 		});
     }
-
-
-
-		
-
-
     </script>
 </body>
 </html>
